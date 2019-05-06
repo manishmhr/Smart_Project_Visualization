3cf151329549c5ab15e50c64ef82c1659ba875c6
manishmhr2@gmail.com <manishmhr2.com>
2019-05-06 07:42:25 +0545

    Working in UserModel.js

---
+++ model/UserModel.js
@@ -0,0 +1,17 @@
+var db = require('..configs/databaseconfig')
+
+const User = sequelize.define('user', {
+    // attributes
+    firstName: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    lastName: {
+        type: Sequelize.STRING
+        // allowNull defaults to true
+    }
+}, {
+    // options
+    freezetableName:
+    tableName:
+});
